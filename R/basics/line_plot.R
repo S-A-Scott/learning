41c0e5411784beb46112bf7e3bde6f7a94c427d2
Spencer Scott <charlesgranjor@126.com>
2015-09-11 19:44:15 +0800

    add R codes

--- R/basics/line_plot.R
+++ R/basics/line_plot.R
@@ -10,7 +10,15 @@ par(pin=c(2, 3))
 par(lwd=2, cex=1.5)
 par(cex.axis=0.75, font.axis=3)
 
-plot(dose, drugA, type="b", pch=19, lty=2, col="red")
-plot(dose, drugB, type="b", pch=23, lty=6, col="blue", bg="green")
+plot(dose, drugA, type="b", pch=19, lty=2, col="red",
+	 main="Clinical Trials for Drug A",
+	 sub="This is hypothetical data",
+	 xlab="Dosage", ylab="Drug Response",
+	 xlim=c(0, 60), ylim=c(0, 70))
+plot(dose, drugB, type="b", pch=23, lty=6, col="blue", bg="green",
+	 main="Clinical Trials for Drug B",
+	 sub="This is hypothetical data",
+	 xlab="Dosage", ylab="Drug Response",
+	 xlim=c(0, 60), ylim=c(0, 70))
 # restore the original graphical parameter settings
 par(opar)

4fcac4153eb628757f18ceb0b88f414d9ae0f20b
Spencer Scott <charlesgranjor@126.com>
2015-09-12 23:14:42 +0800

    add fish simple theme

---
+++ fish/theme-simple/fish_prompt.fish
@@ -0,0 +1,6 @@
+function fish_prompt
+	set_color red
+	echo "⋊>"
+	set_color normal
+	echo " "
+end
---
+++ fish/theme-simple/fish_right_prompt.fish
@@ -0,0 +1,6 @@
+function fish_right_prompt
+	set_color red
+	printf '%s ' (prompt_pwd)
+	echo (prompt_pwd)
+	set_color normal
+end
